7d9af4e0224b07ae0962cddce8a3b0a0ddb3143e
Jędrzej <50214379+jedrzejme@users.noreply.github.com>
2022-08-10 18:12:14 +0200

    Improve length of the code

--- main.py
+++ main.py
@@ -1,69 +1,44 @@
 from random import randint
 from contextlib import redirect_stdout
 
+def generate_phone_number(template):
+    exec(f"""print(f"{template}")""")
+
+def generate_output(template):
+    n = int(input("How many phone numbers to generate: "))
+    save_to_file = str(input("Save to file? [y/n] "))
+    if save_to_file == "y":
+        filename = str(input("File name: "))
+        with open(f"{filename}.txt", 'w') as file:
+            with redirect_stdout(file):
+                for i in range(n):
+                    generate_phone_number(template)
+    else:
+        for i in range(n):
+            generate_phone_number(template)
+
+def end():
+    input("Press enter to exit")
+    exit()
+
 country = str(input("Select country [USA/Poland/Custom] "))
 
 if country == "USA":
-    def generate_phone_number():
-        print(f"+1 ({randint(0,9)}{randint(0,9)}{randint(0,9)}){randint(0,9)}{randint(0,9)}{randint(0,9)}-{randint(0,9)}{randint(0,9)}{randint(0,9)}{randint(0,9)}")
-
-    n = int(input("How many phone numbers to generate: "))
-    save_to_file = str(input("Save to file? [y/n] "))
-    if save_to_file == "y":
-        filename = str(input("File name: "))
-        with open(f"{filename}.txt", 'w') as file:
-            with redirect_stdout(file):
-                for i in range(n):
-                    generate_phone_number()
-    else:
-        for i in range(n):
-            generate_phone_number()
-
-    input("Press enter to exit")
-    exit()
+    generate_output("+1 ({randint(0,9)}{randint(0,9)}{randint(0,9)}){randint(0,9)}{randint(0,9)}{randint(0,9)}-{randint(0,9)}{randint(0,9)}{randint(0,9)}{randint(0,9)}")
+    end()
 
 if country == "Poland":
-    def generate_phone_number():
-        print(f"+48 {randint(1,9)}{randint(0,9)}{randint(0,9)} {randint(0,9)}{randint(0,9)}{randint(0,9)} {randint(0,9)}{randint(0,9)}{randint(0,9)}")
-
-    n = int(input("How many phone numbers to generate: "))
-    save_to_file = str(input("Save to file? [y/n] "))
-    if save_to_file == "y":
-        filename = str(input("File name: "))
-        with open(f"{filename}.txt", 'w') as file:
-            with redirect_stdout(file):
-                for i in range(n):
-                    generate_phone_number()
-    else:
-        for i in range(n):
-            generate_phone_number()
-
-    input("Press enter to exit")
-    exit()
+    generate_output("+48 {randint(1,9)}{randint(0,9)}{randint(0,9)} {randint(0,9)}{randint(0,9)}{randint(0,9)} {randint(0,9)}{randint(0,9)}{randint(0,9)}")
+    end()
 
 if country == "Custom":
-    def generate_phone_number():
-        exec(f"""print(f"{template}")""")
-    
     filename = str(input("File name with custom template: "))
     file = open(f"{filename}.txt","r")
     template = file.read()
 
-    n = int(input("How many phone numbers to generate: "))
-    save_to_file = str(input("Save to file? [y/n] "))
-    if save_to_file == "y":
-        filename = str(input("File name: "))
-        with open(f"{filename}.txt", 'w') as file:
-            with redirect_stdout(file):
-                for i in range(n):
-                    generate_phone_number()
-    else:
-        for i in range(n):
-            generate_phone_number()
+    generate_output(template)
 
-    input("Press enter to exit")
-    exit()
+    end()
 
 print("Wrong country")
-input("Press enter to exit")
-exit()
+end()
