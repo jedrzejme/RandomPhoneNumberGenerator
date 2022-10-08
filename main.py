b3ba6d275bd7f6d4f6b3eff29cc4a35b2bbdd09e
Jędrzej <50214379+jedrzejme@users.noreply.github.com>
2022-10-09 00:16:52 +0200

    Check if filename contains .txt [1.3.0]

--- main.py
+++ main.py
@@ -9,13 +9,20 @@ def generate_output(template):
     save_to_file = str(input("Save to file? [y/n] "))
     if save_to_file == "y":
         filename = str(input("File name: "))
-        with open(f"{filename}.txt", 'w') as file:
-            with redirect_stdout(file):
-                for i in range(n):
-                    generate_phone_number(template)
+        if ".txt" in filename:
+            with open(f"{filename}", 'w') as file:
+                with redirect_stdout(file):
+                    for i in range(n):
+                        generate_phone_number(template)
+        else:
+            with open(f"{filename}.txt", 'w') as file:
+                with redirect_stdout(file):
+                    for i in range(n):
+                        generate_phone_number(template)
     else:
         for i in range(n):
             generate_phone_number(template)
+    end()
 
 def end():
     input("Press enter to exit")
@@ -25,20 +32,19 @@ country = str(input("Select country [USA/Poland/Custom] "))
 
 if country == "USA":
     generate_output("+1 ({randint(0,9)}{randint(0,9)}{randint(0,9)}){randint(0,9)}{randint(0,9)}{randint(0,9)}-{randint(0,9)}{randint(0,9)}{randint(0,9)}{randint(0,9)}")
-    end()
 
 if country == "Poland":
     generate_output("+48 {randint(1,9)}{randint(0,9)}{randint(0,9)} {randint(0,9)}{randint(0,9)}{randint(0,9)} {randint(0,9)}{randint(0,9)}{randint(0,9)}")
-    end()
 
 if country == "Custom":
     filename = str(input("File name with custom template: "))
-    file = open(f"{filename}.txt","r")
+    if ".txt" in filename:
+        file = open(f"{filename}","r")
+    else:
+        file = open(f"{filename}.txt","r")
     template = file.read()
 
     generate_output(template)
-
-    end()
 
 print("Wrong country")
 end()
